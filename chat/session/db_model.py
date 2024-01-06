3d7a22c9054a8419ea73ce6748f1774a951e9339
zkhCreator <zkh90644@gmail.com>
2024-01-06 11:35:50 +0800

    feat: update model

--- chat/session/db_model.py
+++ chat/session/db_model.py
@@ -4,11 +4,10 @@ from common.db_model import BaseDBModel
 from sqlalchemy_utils import UUIDType
 from sqlalchemy.dialects.mysql import TEXT
 
+
 class ChatSessionDBModel(BaseDBModel):
     __tablename__ = 'chat_session'
 
     user_id = Column(UUIDType(binary=False), index=True)
-    assistant_id = Column(UUIDType(binary=False), index=True)
+    role = Column(Column(String(255), nullable=False))
     message = Column(TEXT, nullable=False)
-    params = Column(TEXT, nullable=False)
-    response = Column(TEXT, nullable=False)
